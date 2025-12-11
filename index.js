3ace0209c4e2c59bc463c5926621d2ffeb8ef322
devDouqle <erikbiro7@gmail.com>
2025-12-11 09:25:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+app.use(express.json());// CORS engedélyezés
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 
 app.get("/", (req, res) => res.send("Bot online"));
 
